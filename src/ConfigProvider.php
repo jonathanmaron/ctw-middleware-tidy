dc60dbeebb490ee32bc99dba7402ca73908e6183
Jonathan Maron <jonathan.maron@theimagingsource.com>
2021-03-13 14:47:54 +0100

    Added ConfigProvider

---
+++ src/ConfigProvider.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Ctw\Middleware\TidyMiddleware;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+        ];
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            'aliases'    => [],
+            'invokables' => [],
+            'factories'  => [
+                TidyMiddleware::class => TidyMiddlewareFactory::class,
+            ],
+        ];
+    }
+}
