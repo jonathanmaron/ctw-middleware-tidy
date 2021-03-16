d5e123bbf8bc75688edf95dbb5be6f6ac7ab8a93
Jonathan Maron <jonathan.maron@theimagingsource.com>
2021-03-16 10:24:38 +0100

    Improved tests

---
+++ test/ConfigProviderTest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace CtwTest\Middleware\TidyMiddleware;
+
+use Ctw\Middleware\TidyMiddleware\ConfigProvider;
+use Ctw\Middleware\TidyMiddleware\TidyMiddleware;
+use Ctw\Middleware\TidyMiddleware\TidyMiddlewareFactory;
+
+class ConfigProviderTest extends AbstractCase
+{
+    public function testConfigProvider(): void
+    {
+        $configProvider = new ConfigProvider();
+
+        $expected = [
+            'dependencies' => [
+                'factories' => [
+                    TidyMiddleware::class => TidyMiddlewareFactory::class,
+                ],
+            ],
+        ];
+
+        $this->assertSame($expected, $configProvider->__invoke());
+    }
+}
